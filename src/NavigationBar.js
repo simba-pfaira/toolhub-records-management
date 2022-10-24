2f6d7c1aec6011943357313cd9036c0e58344f8a
Simba Pfaira <simplexpfaira@gmail.com>
2022-10-25 00:57:33 +0200

    navigationbar: Add a navigation bar

---
+++ src/NavigationBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <nav>
+        <h2>Toolhub Records Management</h2>
+        <ul>
+          <li>
+            <a href='/records-dashboard'>Records Dashboard</a>
+          </li>
+          <li>
+            <a href='/leaderboard'>Leaderboard</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
