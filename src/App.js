2a2625e6349c93cccaf6d668113273cdc507882f
Simba Pfaira <simplexpfaira@gmail.com>
2022-10-25 01:00:50 +0200

    app: Add react-router-dom for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Leaderboard from './Leaderboard';
 import NavigationBar from './NavigationBar';
+import RecordsDashboard from './RecordsDashboard';
 
 function App() {
   return (
     <div className='App'>
-      <NavigationBar />
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='leaderboard' element={<Leaderboard />} />
+          <Route path='records-dashboard' element={<RecordsDashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
